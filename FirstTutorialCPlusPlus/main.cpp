19e6aadd154919b96737b0a5b364220b47f20658
Husam Nujaim <husam.nujaim@gmail.com>
2020-11-11 00:53:01 +0100

    Circle Area & Circumference

--- FirstTutorialCPlusPlus/main.cpp
+++ FirstTutorialCPlusPlus/main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include<conio.h>
+#include <cmath>
 
-using namespace std;
-
-int main()
+double * CircleMath(int diameter)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+double Area,Circumference;
+Area = (3.14 * pow(diameter,2)) / 4; //Area = (pi * diameter^2) / 4
+Circumference = 3.14*diameter;       //Circumference =  pi * diameter
+double static  circleMath[2];
+circleMath[0] = Area;
+circleMath[1]= Circumference;
+return circleMath;
+}
+
+int main(void)
+{
+    double diameter;
+    std::cout<<"Enter the diameter of the circle: ";
+    std::cin>>diameter;
+    std::cout<<std::endl;
+    std::cout<<"The Area = "<<CircleMath(diameter)[0]<<std::endl;
+    std::cout<<"The Circumference = "<<CircleMath(diameter)[1]<<std::endl;
+
+return 0;
 }
