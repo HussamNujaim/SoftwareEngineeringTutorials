d8645191da2a729fcc569fbef60b33286ff18224
Husam Nujaim <husam.nujaim@gmail.com>
2020-11-11 03:09:30 +0100

    Problem 2: Find the max and the min of a table.

--- FirstTutorialCPlusPlus/main.cpp
+++ FirstTutorialCPlusPlus/main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<conio.h>
 #include <cmath>
+#include <vector>
+using namespace std;
 
+
+
+
+// Problem 1:
 double * CircleMath(int diameter)
 {
 double Area,Circumference;
@@ -12,15 +18,69 @@ circleMath[0] = Area;
 circleMath[1]= Circumference;
 return circleMath;
 }
+////////////////////////////////////////
+
+// Problem 2:
+typedef vector< vector<int> > Table;
+int * GetMinMaxValue(Table& table)
+{
+    int numberOfRows=table.size();
+    int numberOfCoulumns=table[0].size();
+    //Finding the maximum value
+    int max= table[0][0]; //Choosing the first element as the maximum value initially
+    int min = table[0][0]; //Choosing the first element as the minimum value initially
+    for(int i=0;i<numberOfRows;i++)
+    {
+        for(int j=0;j<numberOfCoulumns;j++)
+        {
+            if(max<table[i][j])  // if the current value is greater than the old value, then change the old value with the current one
+                max=table[i][j];
+            if(min>table[i][j]) // if the current value is less than the old value, then change the old value with the current one
+                min=table[i][j];
+        }
+    }
+    int MinMax[2] = {min,max};
+return MinMax;
+}
+//////////////////////////////////////////
 
 int main(void)
 {
-    double diameter;
+    // Problem 1:
+   /* double diameter;
     std::cout<<"Enter the diameter of the circle: ";
     std::cin>>diameter;
     std::cout<<std::endl;
     std::cout<<"The Area = "<<CircleMath(diameter)[0]<<std::endl;
     std::cout<<"The Circumference = "<<CircleMath(diameter)[1]<<std::endl;
+   */
+    // Problem 2:
+    int numberOfRows, numberOfCoulumns;
+    std::cout<<"Enter the number of rows: ";
+    std::cin>>numberOfRows;
+    std::cout<<"Enter the number of columns: ";
+    std::cin>>numberOfCoulumns;
+    std::cout<<std::endl;
+   // int table[numberOfRows][numberOfCoulumns];
+    //Table initialization
+    Table table( numberOfRows,vector<int>(numberOfCoulumns,0));
 
+    for(int i=0;i<numberOfRows;i++)
+    {
+        for(int j=0;j<numberOfCoulumns;j++)
+        {
+            table[i][j]=(rand() % 99) + 1;  // Initializing random values to the table
+            std::cout<<table[i][j]<<" "; // Displaying the values of the table
+        }
+        std::cout<<std::endl;
+    }
+    std::cout<<std::endl;
+    int min= GetMinMaxValue(table)[0];
+    int max= GetMinMaxValue(table)[1];
+    std::cout<<"The Minimum value is = " << min;
+    std::cout<<std::endl;
+    std::cout<<"The Maximum value is = " << max;
+    std::cout<<std::endl;
 return 0;
 }
+
